15d9ea4cdd80e08207b53e35c3979d671d6497db
Brandon Graham <bgraham452@gmail.com>
2018-06-13 12:40:42 -0400

    changed objectCreator.js to objectDrills1.js, created objectDrills2.js, and completed the makeStudentReport function

---
+++ objectDrills2.js
@@ -0,0 +1,66 @@
+'use strict';
+
+function makeStudentsReport(data) {
+let strArray = [];
+for (const key in data) { 
+    const value = data[key]; 
+    //console.log(value.name); 
+    strArray.push(`${value.name}: ${value.grade}`);
+    //console.log(strArray);
+}
+return strArray;
+}
+  
+
+/* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     Nothing to see here!
+     
+  */
+  
+// tests
+  
+function testIt() {
+  const testData = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
+  ];
+  
+  const expectations = ['Jane Doe: A', 'John Dough: B', 'Jill Do: A'];
+  
+  const results = makeStudentsReport(testData);
+  
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `makeStudentsReport` must return an array');
+    return;
+  }
+  if (results.length !== testData.length) {
+    console.error(
+      'FAILURE: test data had length of ' +
+          testData.length +
+          ' but `makeStudentsReport` returned array of length ' +
+          results.length
+    );
+    return;
+  }
+  for (let i = 0; i < expectations.length; i++) {
+    let expect = expectations[i];
+    if (
+      !results.find(function(item) {
+        return item === expect;
+      })
+    ) {
+      console.error(
+        'FAILURE: `makeStudentsReport` is not ' + 'producing expected strings'
+      );
+      return;
+    }
+  }
+  console.log('SUCCESS: `makeStudentsReport` is working');
+}
+  
+testIt();
+
+  
