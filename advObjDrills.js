3f8d81617d4bb8ebd8ff80d208d0317a3cd803cb
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-13 14:34:57 -0400

    Finished Iterating Drill

--- advObjDrills.js
+++ advObjDrills.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// 1. Object Initializers and Methods
 
 const loaf = {
   flour: 300,
@@ -13,3 +14,23 @@ const loaf = {
 console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration());
+
+// 2. Iterating over an object's properties
+
+const testObj = {
+  foo: 'hm',
+  bar: 'oh',
+  fum: 'look',
+  quux: 'listen',
+  spam: 'see'
+}
+
+const loopThrough = function(obj) {
+  for (const key in obj) {
+    console.log(key);
+    const value = obj[key];
+    console.log(value);
+  }
+};
+
+loopThrough(testObj);
