a974c36ca130cbd0a5f864bc71f34f1e57cd8103
Brandon Graham <bgraham452@gmail.com>
2018-06-13 12:12:36 -0400

    create the self-reference function

--- objectCreator.js
+++ objectCreator.js
@@ -70,10 +70,10 @@ createMyObject();
 // 2. Object updater
 
 function updateObject(obj) {
-  obj.foo = "foo";
-  obj.bar = "bar";
-  obj.bizz = "bizz";
-  obj.bang = "bang";
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
 
   return obj;
 }
@@ -96,7 +96,7 @@ function updateObject(obj) {
   var newObj = updateObject(oldObj);
   if (typeof newObj !== 'object') { 
     console.error('ERROR: `createMyObject` must return an object');
-    return false
+    return false;
   }
   ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
     if (!(key in newObj)) {
@@ -117,3 +117,62 @@ function updateObject(obj) {
   console.log('SUCCESS: `updateObject` works correctly!');
   
 })();
+
+
+
+// 3. Self-reference
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    //console.log(firstName);
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    
+    fullName: function(){
+        let fullName = `${this.firstName} ${this.lastName}`
+        return fullName;
+    }
+  };
+  return person;
+}
+  
+/* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+(function testPersonMaker() {
+  var person = personMaker();
+  if (typeof person !== 'object') {
+    console.error('ERROR: `personMaker` must return an object');
+    return false;
+  }
+  if (typeof person.fullName !== 'function') {
+    console.error('ERROR: `fullName` must be a method');
+    return false;
+  }
+  if (person.fullName() !== 'Paul Jones') {
+    console.error(
+      'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
+          person.fullName()
+    );
+    return false;
+  }
+  person.firstName = 'Lisa';
+  person.lastName = 'Simpson';
+  if (person.fullName() !== 'Lisa Simpson') {
+    console.error(
+      '`personMaker` is not using self reference correctly. ' +
+          'When firstName set to "Lisa" and lastName set to "Simpson", ' +
+          'should return "Lisa Simpson" but returned ' +
+          person.fullName()
+    );
+  }
+  console.log('SUCCESS: `updateObject` works correctly!');
+})();
+  
